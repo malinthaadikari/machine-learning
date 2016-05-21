64321526b01234d1f036dc519c7bfd94b0091d5c
Malintha <malintha@wso2.com>
2016-05-21 16:20:43 +0530

    Improved code

--- KMeansCLutering.py
+++ KMeansCLutering.py
@@ -4,14 +4,13 @@ import Utils
 
 if __name__ == '__main__':
     df = Utils.get_dataframe("crime_data.csv")
-    print(df.shape)
-    kmeans_model = KMeans(n_clusters=2, random_state=1)
+    KMeans_model = KMeans(n_clusters=5, random_state=1)
     df.drop(['crime$cluster'], inplace=True, axis=1)
     df.rename(columns={df.columns[0]: 'State'}, inplace=True)
     print(df.head(), end="\n\n")
     numeric_columns = df._get_numeric_data()
-    kmeans_model.fit(numeric_columns)
-    labals = kmeans_model.labels_
-    centroids = kmeans_model.cluster_centers_
-    print(labals)
-    print(kmeans_model.predict([[15, 236, 58, 21.2]]))
+    KMeans_model.fit(numeric_columns)
+    labels = KMeans_model.labels_
+    centroids = KMeans_model.cluster_centers_
+    print(labels)
+    print(KMeans_model.predict([[15, 236, 58, 21.2]]))
