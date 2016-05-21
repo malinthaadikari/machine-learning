69a93b2c73a5d51c49d675b90d367180c9f8b4b5
Malintha <malintha@wso2.com>
2016-05-21 09:56:55 +0530

    Adding K-Means-Clustering

---
+++ K-Means-Clustering/KMeansCLutering.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+import pandas as pand
+from sklearn.cluster import KMeans
+
+
+def load_data():
+    df = pand.read_csv("/home/malintha/PycharmProjects/HelloWorld/crime_data.csv")
+    return df
+
+
+if __name__ == '__main__':
+    df = load_data()
+    print(df.shape)
+    kmeans_model = KMeans(n_clusters=2, random_state=1)
+    df.drop(['crime$cluster'], inplace=True, axis=1)
+    df.rename(columns={df.columns[0]: 'State'}, inplace=True)
+    print(df.head(), end="\n\n")
+    numeric_columns = df._get_numeric_data()
+    kmeans_model.fit(numeric_columns)
+    labals = kmeans_model.labels_
+    centroids = kmeans_model.cluster_centers_
+    print(labals)
+    print(kmeans_model.predict([[15, 236, 58, 21.2]]))
